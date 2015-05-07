f7ce9c1196bbb135afbe9734a5515f65404f394f
Radu Ioan Fericean <radu@fericean.ro>
2015-05-07 18:59:46 +0300

    fixes and better file index

--- imgdup.py
+++ imgdup.py
@@ -3,14 +3,14 @@
 
 from PIL import Image
 from glob import glob
-import sys, shutil, os
-import argparse
+from hashlib import md5
+import sys, shutil, os, argparse
 
 DUP_FOLDER = 'duplicates'
 KEEP_SUFIX = '_KEPT_'
 DELETE_SUFIX = '_GONE_'
-KEEP = '%d'+KEEP_SUFIX
-DELETE = '%d'+DELETE_SUFIX
+KEEP = '%s'+KEEP_SUFIX
+DELETE = '%s'+DELETE_SUFIX
 
 
 def dhash(image, hash_size = 8):
@@ -42,6 +42,7 @@ def dhash(image, hash_size = 8):
 
     return ''.join(hex_string)
 
+#not used yet, can be used to compare dict values
 def hamming_distance(s1, s2):
     #Return the Hamming distance between equal-length sequences
     if len(s1) != len(s2):
@@ -94,7 +95,9 @@ if __name__ == '__main__':
                 file_name = img_path.split(DELETE_SUFIX)[-1]
                 shutil.move(img_path, file_name)
                 print('recovered', file_name)
-        os.rmdir(DUP_FOLDER)
+        try:
+            os.rmdir(DUP_FOLDER)
+        except OSError: pass
         sys.exit()
     
     img_dict = {}
@@ -104,32 +107,33 @@ if __name__ == '__main__':
     for files in types: images.extend(glob(files))
     
     i = 0
-    d = 0
     for img_path in images:        
         sys.stdout.write("\r%d%%" % (i*100/len(images)))
         sys.stdout.flush()
         i+=1
         try:
-	        img = Image.open(img_path)
-	        
-	        comp = getattr(sys.modules[__name__], args.cmp) if type(args.cmp) is str else args.cmp
+            img = Image.open(img_path)
+        
+            comp = getattr(sys.modules[__name__], args.cmp) if type(args.cmp) is str else args.cmp
 	
-	        ii1 = ImgInfo(img_path, img.size, comp)
-	        a = dhash(img)
-	        if a in img_dict:
-	            if not os.path.exists(DUP_FOLDER) and not args.dry_run: os.mkdir(DUP_FOLDER)
-	            ii2 = img_dict[a]
-	            if not args.dry_run:
-	                if compa(ii1, ii2, args.invert):
-	                    shutil.copy(ii1.name, os.path.join(DUP_FOLDER, KEEP % d + ii1.name))
-	                    shutil.move(ii2.name, os.path.join(DUP_FOLDER, DELETE % d + ii2.name))
-	                else:
-	                    shutil.move(ii1.name, os.path.join(DUP_FOLDER, DELETE % d + ii1.name))
-	                    shutil.copy(ii2.name, os.path.join(DUP_FOLDER, KEEP % d + ii2.name))	            
+            ii1 = ImgInfo(img_path, img.size, comp)
+            a = dhash(img)
+            
+            if a in img_dict:                
+                if not os.path.exists(DUP_FOLDER) and not args.dry_run: os.mkdir(DUP_FOLDER)
+                ii2 = img_dict[a]
+                if not args.dry_run:
+                    prefix = md5((ii1.name + ii2.name).encode('utf-8')).hexdigest()[:5]
+                    if compa(ii1, ii2, args.invert):
+                        shutil.copy(ii1.name, os.path.join(DUP_FOLDER, KEEP % prefix + ii1.name))
+                        shutil.move(ii2.name, os.path.join(DUP_FOLDER, DELETE % prefix + ii2.name))
+                        img_dict[a] = ii1
+                    else:
+                        shutil.move(ii1.name, os.path.join(DUP_FOLDER, DELETE % prefix + ii1.name))
+                        shutil.copy(ii2.name, os.path.join(DUP_FOLDER, KEEP % prefix + ii2.name))
                     print("\r%s and %s are too similar" % (ii2.name, ii1.name))
-	            d += 1
-	        else:
-	            img_dict[a] = ii1
+            else:
+                img_dict[a] = ii1
         except:
-            print ("error processing file %s: %s"% (img_path, sys.exc_info()[0]))
+            print ("error processing files (%s, %s): %s" % (ii1.name, ii2.name, sys.exc_info()))
 
